0cd7dc9bf383936f6ca055ef3ea1f0c27682d892
ENMN11 <tranbao0975@gmail.com>
2025-09-05 12:15:24 +0700

    Update S11.py

--- S11.py
+++ S11.py
@@ -165,12 +165,12 @@ mt_path = os.path.join(DEST_DIR, "MTManager.apk")
 download(f"{BASE_URL}/MTManager.apk", mt_path)
 install(mt_path)
 
-print(TITLE + "🎯 Installing 8 Global Tabs...")
-files = [f"{i}.apk" for i in range(1, 9)]
+print(TITLE + "🎯 Installing 9 Global Tabs...")
+files = [f"{i}.apk" for i in range(1, 10)]
 paths = [os.path.join(DEST_DIR, f) for f in files]
 
 with ThreadPoolExecutor(max_workers=4) as ex:
-    [f.result() for f in as_completed([ex.submit(download, f"{BASE_URL}/Delta/{fn}", p) for fn, p in zip(files, paths)])]
+    [f.result() for f in as_completed([ex.submit(download, f"{BASE_URL}/LiteN/{fn}", p) for fn, p in zip(files, paths)])]
     print(SUCCESS + "🎯 All Global Tabs Downloaded!")
 
 with ThreadPoolExecutor(max_workers=4) as ex:
